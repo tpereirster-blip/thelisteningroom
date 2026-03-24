8f236f942a220efa29687604476ae1afd4eb889a
tpereirster-blip <tpereirster-blip@users.noreply.github.com>
2026-03-24 16:48:53 -0700

    Fix album cover images: use remotePatterns for mzstatic.com

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.mzstatic.com',
+      },
+    ],
+  },
+}
 module.exports = nextConfig
